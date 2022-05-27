8fc47342ffee1235722f66cc4a17a6d416bf67a2
Mohammad Ghazanfari <mgh.5225@gmail.com>
2022-05-27 21:09:56 +0430

    React-Toastify added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { MainRouter } from "./routes";
+import { ToastContainer } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <MainRouter />;
+  return (
+    <div>
+      <MainRouter />
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
