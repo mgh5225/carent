95d073de770d215e115a9733211d4b08a9c6754e
NamiNaziri <naminaziri1@gmail.com>
2022-05-30 10:15:13 +0430

    Initial login page added

---
+++ src/components/Login/Login.js
@@ -0,0 +1,129 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { connect } from "react-redux";
+
+import { login } from "utils/users";
+import { toast } from "react-toastify";
+
+const LoginComponent = (props) => {
+
+  
+  const handleSubmit =  async (event) => {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+
+    const { Login } = props;
+    const { user, pass } = {user:data.get('username'), pass: data.get('password')};
+
+    console.log("LoginComponent: onFormSubmitted");
+
+
+    console.log({
+      user,
+      pass,
+    });
+
+
+    try {
+      const { message } = await Login(user, pass);
+      toast.success(message);
+    } catch (err) {
+      const message = err.response
+        ? err.response.data.message
+        : "Something went wrong! Please try again later";
+      toast.error(message);
+    }
+  };
+
+
+ // 
+  return (
+
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            ورود
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="ایمیل"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="رمز عبور"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              ورود
+            </Button>
+            <Grid container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            >
+              <Grid
+              
+              item>
+                
+                <Link to="/" variant="body2">
+
+                  {"اکانت نداری؟ خو ثبتنام کن:)"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    isAuth: state?.auth?.isAuthenticated,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    Login: (username, password) => dispatch(login({ username, password })),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginComponent);
+
+
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,57 +1,25 @@
 import React from "react";
-import { connect } from "react-redux";
 
-import { login } from "utils/users";
-import { toast } from "react-toastify";
+import LoginComponent  from "../../components/Login/Login";
+import Container from '@mui/material/Container';
 
 class LoginPage extends React.Component {
-  onFormSubmitted = async (e) => {
-    e.preventDefault();
 
-    const { loginkon } = this.props;
-    const { user, pass } = e.target.elements;
 
-    try {
-      const { message } = await loginkon(user.value, pass.value);
-      toast.success(message);
-    } catch (err) {
-      const message = err.response
-        ? err.response.data.message
-        : "Something went wrong! Please try again later";
-      toast.error(message);
-    }
-  };
 
   render() {
     const { isAuth } = this.props;
 
-    return (
-      <div>
-        {!!isAuth ? "You are authenticated" : "you are not authenticated"}
-        <form onSubmit={this.onFormSubmitted}>
-          <input
-            name="user"
-            defaultValue="nami"
-            placeholder="enter your username"
-          />
-          <input name="pass" placeholder="Enter your password" />
-          <button type="submit">Login</button>
-        </form>
-      </div>
-    );
+    return(
+
+      <Container component="main" maxWidth="xs">
+        
+        <LoginComponent onLoginSubmit={this.onFormSubmitted}></LoginComponent>
+      </Container>
+      
+    )
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    isAuth: state?.auth?.isAuthenticated,
-  };
-};
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    loginkon: (username, password) => dispatch(login({ username, password })),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
+export default LoginPage;
