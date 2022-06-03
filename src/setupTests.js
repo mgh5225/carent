3e77eb9727ccd7ab93702d8dd045458b5a34a3e0
Mohammad Ghazanfari <mgh.5225@gmail.com>
2022-06-03 18:48:24 +0430

    setup test

---
+++ src/setupTests.js
@@ -0,0 +1,21 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom";
+import { store } from "./store";
+import { Provider } from "react-redux";
+
+import React from "react";
+import { render as rtlRender } from "@testing-library/react";
+
+function render(ui, renderOptions) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+export * from "@testing-library/react";
+export { render };
